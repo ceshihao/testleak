90f692307794a7ca0373ebdd73c0287e6ab155bd
ceshihao <davidzheng23@gmail.com>
2017-11-24 12:01:58 +0800

    make gofmt happy

--- testleak_test.go
+++ testleak_test.go
@@ -5,33 +5,33 @@ import (
 	"time"
 )
 
-func TestLeakNoGoroutine(t *testing.T){
-	defer TestLeak(t) ()
+func TestLeakNoGoroutine(t *testing.T) {
+	defer TestLeak(t)()
 	func() {
 		time.Sleep(5000 * time.Millisecond)
-	} ()
+	}()
 }
 
-func TestLeakShortGoroutine(t *testing.T){
-	defer TestLeak(t) ()
+func TestLeakShortGoroutine(t *testing.T) {
+	defer TestLeak(t)()
 	go func() {
 		time.Sleep(5 * time.Millisecond)
-	} ()
+	}()
 }
 
-func TestLeakLongGoroutine(t *testing.T){
+func TestLeakLongGoroutine(t *testing.T) {
 	// demo Goroutine leak failure case by comment the following line
 	t.Skip("Skip this case because Goroutine leak failure")
-	defer TestLeak(t) ()
+	defer TestLeak(t)()
 	go func() {
 		time.Sleep(5000 * time.Millisecond)
-	} ()
+	}()
 }
 
-func TestLeakAppendWhiteList(t *testing.T){
+func TestLeakAppendWhiteList(t *testing.T) {
 	AppendTestLeakWhiteList("time.Sleep(")
-	defer TestLeak(t) ()
+	defer TestLeak(t)()
 	go func() {
 		time.Sleep(5000 * time.Millisecond)
-	} ()
+	}()
 }
